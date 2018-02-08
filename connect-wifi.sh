f52d2eaf26cd944950259846d02d0ca3c7920e54
hiro <wfy11235813@gmail.com>
2018-02-08 09:44:53 +0800

    添加录制视频&音频的script

--- connect-wifi.sh
+++ connect-wifi.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # connect the wifi when give a name and a password
 
-nmcli dev wifi connect nicehiro password w1234567
+# nmcli dev wifi connect nicehiro password w1234567
+
+nmcli dev wifi connect 大神的WIFI password 11235813wfy
 
 ## nmcli r wifi off
 ## nmcli r wifi on
