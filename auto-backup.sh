51976bc9602f59115c83cef34075b3432a10e3d8
hiro <wfy11235813@gmail.com>
2019-02-01 16:08:11 +0800

    add v2ray config

--- auto-backup.sh
+++ auto-backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # auto backup through git
+# useage: crontab -h
 
-cd /home/hiro/org/
+cd /home/hiro/org-files/
 git add .
 git commit -m "Backup taken at @ $(date)"
 git push
 
 cd /home/hiro/bin/
+git add .
+git commit -m "Backup taken at @ $(date)"
+
+cd /home/hiro/sicp/
 git add .
 git commit -m "Backup taken at @ $(date)"
